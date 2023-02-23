d4a902ad45ee8b77edeea164543df09d2eaf8c57
A.Ottr <me@staubrein.com>
2023-02-23 03:48:47 +0100

    update linting

--- src/Client.ts
+++ src/Client.ts
@@ -1,38 +1,50 @@
 export default class Client {
+  baseUrl: string;
 
-    baseUrl: string;
-    token: string | undefined;
+  token: string | undefined;
 
-    constructor(baseUrl = 'https://shadedoes3d.com/api', token = '') {
-        this.baseUrl = baseUrl;
-        this.token = token;
+  constructor(baseUrl = 'https://shadedoes3d.com/api', token = '') {
+    this.baseUrl = baseUrl;
+    this.token = token;
+  }
+
+  /* eslint @typescript-eslint/no-unsafe-return: "off" */
+  async getToken(username: string, password: string): Promise<string | null> {
+    interface ErrorResponse {
+      status: string;
+      message: string;
     }
 
-    async getToken(username: string, password: string) {
-        let config = {
-            method: 'POST',
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
-                username: username,
-                password: password
-            })
+    interface SuccessResponse {
+      status: string;
+      data: any;
+    }
+
+    const config = {
+      method: 'POST',
+      headers: {
+        /* eslint quote-props: "off" */
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        username,
+        password,
+      }),
+    };
+
+    return fetch(`${this.baseUrl}/auth`, config)
+      .then(async (response) => {
+        try {
+          const resp = await response.json() as SuccessResponse;
+          if (resp.status === 'success') {
+            /* eslint @typescript-eslint/no-unsafe-member-access: "off" */
+            return resp.data.token as string;
+          }
+        } catch (_) {
+          console.log('no');
         }
-
-        return fetch(`${this.baseUrl}/auth`, config)
-            .then(async (response) => {
-                let resp: any = {};
-                try {
-                    resp = await response.json();
-                } catch (_) {
-
-                }
-                if (resp.status === 'success')
-                    return resp.data.token;
-
-                return null;
-            });
-    }
+        return null;
+      });
+  }
 }
--- src/__tests__/Client.test.ts
+++ src/__tests__/Client.test.ts
@@ -1,7 +1,7 @@
 import Client from '../Client';
 test('Login', async () => {
-    const client = new Client();
-    //const token = await client.getToken('alex', '*******');
-    //expect(token).not.toBeNull()
-    expect(await client.getToken('TestUser', 'WrongPassword')).toBeNull()
+  const client = new Client();
+  //const token = await client.getToken('alex', '*******');
+  //expect(token).not.toBeNull()
+  expect(await client.getToken('TestUser', 'WrongPassword')).toBeNull();
 });
